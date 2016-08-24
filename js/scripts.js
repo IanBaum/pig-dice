c2f19c6a83c9e65fa70b52f95aad52f9ef42a467
Ian Baum and Rebecca Allen <rebecca@>
2016-08-24 10:20:23 -0400

    add dice roll prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
+//business logic
+var Player = function() {
+  this.score = 0;
+}
+
+var Dice = function() {
+  this.side = 1;
+  this.total = 0;
+}
+
+Dice.prototype.roll = function(){
+  return Math.ceil(Math.random()*6);
+}
+
+
+
+//user logic
+
 $(document).ready(function(){
-  
+  var dice = new Dice();
+
+  $("#twoPlayer").click(function(){
+    $(".pigRules").hide();
+    $(".pigDice").show();
+  });
+
+  $("#diceRoll").click(function(){
+    $("#currentRoll").text(dice.roll());
+  });
+
 });
